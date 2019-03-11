b2c24ce5665eb8e61d3a298e6e7f09e5a4a95afd
olympia <owojcik7@gmail.com>
2019-03-11 13:53:30 -0400

    Add Login form

--- worther/src/App.js
+++ worther/src/App.js
@@ -1,37 +1,32 @@
 import React, { Component } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+import { Route, Link, NavLink } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+
+import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 
 import "./App.css";
+
+//Use Link from ReactRouterDom to link nav bar links to different components
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">Worther</NavbarBrand>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <Link to="/">About us</Link>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
+              <Link to="/">Sign up</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/login">Log In</Link>
             </NavItem>
           </Nav>
         </Navbar>
+        <Route path="/login" component={LoginPage} />
       </div>
     );
   }
