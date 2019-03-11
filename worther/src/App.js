5f87806fcd6bd53618fd8cfd7fed5fcc2162b34d
jdspell <spelljd15@gmail.com>
2019-03-11 13:54:18 -0400

    beginning to create the redux reducer, store and actions

--- worther/src/App.js
+++ worther/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+// LoginForm class 
+// navbar functional
+// homeForm class
+// homeEstimatePage functional
+
+// profilepage functional
+// homesList functional
+// homecard functional
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      
       </div>
     );
   }
---
+++ worther/src/actions/index.js
@@ -0,0 +1,12 @@
+
+// export const GET_USER = "GET_USER";
+// export const LOGIN_FAILED = "LOGIN_FAILED";
+
+// export const ADD_HOME = "ADD_HOME";
+// export const DELETE_HOME = "DELETE_HOME";
+
+// export const loginSuccess = () = {
+//     return {
+//         type: LOGIN_SUCCESS
+//     }
+// }
